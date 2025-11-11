9c0d3ca9819f02ba825b1cb0271e5d7a3b7b2847
WFlyToTheSky <wallyhare20@gmail.com>
2025-11-10 20:11:05 -0500

    Quartz sync: Nov 10, 2025, 8:11 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "Lumpy Spaghetti Space",
-    pageTitleSuffix: "",
+    pageTitleSuffix: " - Lumpy Spaghetti Space",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
